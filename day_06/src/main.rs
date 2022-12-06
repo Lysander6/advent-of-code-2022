d53db41e32935332569371aa0e3fea37b145ba79
Lysander <lysander6@gmail.com>
2022-12-06 14:48:07 +0100

    day 6 part 1 solution

--- day_06/src/main.rs
+++ day_06/src/main.rs
@@ -1,11 +1,29 @@
-use anyhow::Context;
+use anyhow::{anyhow, Context};
 use common::{get_arg, read_file_to_string};
+
+fn find_start_of_packet(packet: &str) -> Option<usize> {
+    let idx = packet
+        .chars()
+        .collect::<Vec<_>>()
+        .windows(4)
+        .position(|window| {
+            let [a, b, c, d]: [char; 4] = window.try_into().unwrap();
+
+            a != b && a != c && a != d && b != c && b != d && c != d
+        });
+
+    idx.map(|i| i + 4)
+}
 
 fn main() -> Result<(), anyhow::Error> {
     let input_file_path = get_arg(1).context("pass path to input file as first argument")?;
     let input_string = read_file_to_string(&input_file_path)?;
 
-    println!("Part 1 solution: {}", 0);
+    println!(
+        "Part 1 solution: {}",
+        find_start_of_packet(&input_string)
+            .ok_or_else(|| anyhow!("couldn't find start of packet"))?
+    );
     println!("Part 2 solution: {}", 0);
 
     Ok(())
@@ -16,5 +34,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_name() {}
+    fn test_find_start_of_packet() {
+        assert_eq!(
+            find_start_of_packet("mjqjpqmgbljsphdztnvjfqwrcgsmlb"),
+            Some(7)
+        );
+        assert_eq!(
+            find_start_of_packet("bvwbjplbgvbhsrlpgdmjqwftvncz"),
+            Some(5)
+        );
+        assert_eq!(
+            find_start_of_packet("nppdvjthqldpwncqszvftbrmjlhg"),
+            Some(6)
+        );
+        assert_eq!(
+            find_start_of_packet("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"),
+            Some(10)
+        );
+        assert_eq!(
+            find_start_of_packet("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"),
+            Some(11)
+        );
+    }
 }
