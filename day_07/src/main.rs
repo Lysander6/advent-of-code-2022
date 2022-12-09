0a3d220f8d407f8fc2f395afec7525c70cd50440
Lysander <lysander6@gmail.com>
2022-12-09 22:47:34 +0100

    reduce unnecessary data copying in day 7 solution

--- day_07/src/main.rs
+++ day_07/src/main.rs
@@ -6,13 +6,13 @@ use common::{get_arg, read_file_to_string};
 const ROOT: &str = "";
 
 /// Constructs mapping of directory paths to their total sizes
-fn dir_walk(commands: &[&str]) -> HashMap<String, u64> {
+fn dir_walk<'a>(commands: impl IntoIterator<Item = &'a str>) -> HashMap<String, u64> {
     // Current working directory
     let mut cwd: Vec<String> = vec![];
     // Directories and their sizes
     let mut dirs: HashMap<String, u64> = HashMap::new();
 
-    for &command in commands {
+    for command in commands {
         if command.starts_with("$ cd") {
             // We leverage the property of input that we only move up or down
             // (out/in) by one directory and never jump around file-system
@@ -57,13 +57,12 @@ fn sum_sizes_of_small_directories(dirs: &HashMap<String, u64>) -> u64 {
         .sum()
 }
 
-fn find_smallest_directory_that_frees_up_enough_space(dirs: &HashMap<String, u64>) -> u64 {
+fn find_smallest_directory_that_frees_up_enough_space(dirs: &HashMap<String, u64>) -> Option<u64> {
     let total_disk_space = 70000000;
     let current_free_space = total_disk_space - dirs[ROOT];
     let space_needed_to_be_freed = 30000000 - current_free_space;
 
-    let mut sizes = dirs
-        .iter()
+    dirs.iter()
         .filter_map(|(_, &size)| {
             if size >= space_needed_to_be_freed {
                 Some(size)
@@ -71,23 +70,19 @@ fn find_smallest_directory_that_frees_up_enough_space(dirs: &HashMap<String, u64
                 None
             }
         })
-        .collect::<Vec<_>>();
-
-    sizes.sort();
-
-    sizes[0]
+        .min()
 }
 
 fn main() -> Result<(), anyhow::Error> {
     let input_file_path = get_arg(1).context("pass path to input file as first argument")?;
     let input_string = read_file_to_string(&input_file_path)?;
 
-    let dirs = dir_walk(&input_string.lines().collect::<Vec<_>>());
+    let dirs = dir_walk(input_string.lines());
 
     println!("Part 1 solution: {}", sum_sizes_of_small_directories(&dirs));
     println!(
         "Part 2 solution: {}",
-        find_smallest_directory_that_frees_up_enough_space(&dirs)
+        find_smallest_directory_that_frees_up_enough_space(&dirs).unwrap()
     );
 
     Ok(())
@@ -124,7 +119,7 @@ $ ls
 
     #[test]
     fn test_dir_walk() {
-        let dirs = dir_walk(&TEST_INPUT.lines().collect::<Vec<_>>());
+        let dirs = dir_walk(TEST_INPUT.lines());
 
         assert_eq!(
             dirs,
@@ -139,7 +134,7 @@ $ ls
 
     #[test]
     fn test_sum_sizes_of_small_directories() {
-        let dirs = dir_walk(&TEST_INPUT.lines().collect::<Vec<_>>());
+        let dirs = dir_walk(TEST_INPUT.lines());
         let sum = sum_sizes_of_small_directories(&dirs);
 
         assert_eq!(sum, 95437);
@@ -147,8 +142,8 @@ $ ls
 
     #[test]
     fn test_find_smallest_directory_that_frees_up_enough_space() {
-        let dirs = dir_walk(&TEST_INPUT.lines().collect::<Vec<_>>());
-        let size = find_smallest_directory_that_frees_up_enough_space(&dirs);
+        let dirs = dir_walk(TEST_INPUT.lines());
+        let size = find_smallest_directory_that_frees_up_enough_space(&dirs).unwrap();
 
         assert_eq!(size, 24933642);
     }
