ae55b7ad5a9ff6ffb4dc198d8e877dfdd425c1a9
Lysander <lysander6@gmail.com>
2022-12-11 11:33:31 +0100

    init day 11

---
+++ day_11/src/main.rs
@@ -0,0 +1,49 @@
+use anyhow::Context;
+use common::{get_arg, read_file_to_string};
+
+fn main() -> Result<(), anyhow::Error> {
+    let input_file_path = get_arg(1).context("pass path to input file as first argument")?;
+    let input_string = read_file_to_string(&input_file_path)?;
+
+    println!("Part 1 solution: {}", 0);
+    println!("Part 2 solution: {}", 0);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "\
+Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1";
+
+    #[test]
+    fn test_name() {}
+}
