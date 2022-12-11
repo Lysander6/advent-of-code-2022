a40604009afe7e766f47b1eaa08b26906ff90c0a
Lysander <lysander6@gmail.com>
2022-12-11 19:11:29 +0100

    day 11 part 2 solution

--- day_11/src/main.rs
+++ day_11/src/main.rs
@@ -3,23 +3,23 @@ use std::{collections::VecDeque, str::FromStr};
 use anyhow::{anyhow, bail, Context};
 use common::{get_arg, read_file_to_string};
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum Either<L, R> {
     Left(L),
     Right(R),
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Old;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 enum Operation {
-    Add(Either<Old, i32>, Either<Old, i32>),
-    Multiply(Either<Old, i32>, Either<Old, i32>),
+    Add(Either<Old, u64>, Either<Old, u64>),
+    Multiply(Either<Old, u64>, Either<Old, u64>),
 }
 
 impl Operation {
-    fn execute(&self, old: i32) -> i32 {
+    fn execute(&self, old: u64) -> u64 {
         use Either::*;
 
         match self {
@@ -65,12 +65,12 @@ impl FromStr for Operation {
 
         let left = match left {
             "old" => Either::Left(Old),
-            v => Either::Right(v.parse::<i32>()?),
+            v => Either::Right(v.parse::<u64>()?),
         };
 
         let right = match right {
             "old" => Either::Left(Old),
-            v => Either::Right(v.parse::<i32>()?),
+            v => Either::Right(v.parse::<u64>()?),
         };
 
         let operation = match operator {
@@ -83,15 +83,15 @@ impl FromStr for Operation {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct MonkeyTest {
-    divisibility_operand: i32,
+    divisibility_operand: u64,
     if_true_receiver: usize,
     if_false_receiver: usize,
 }
 
 impl MonkeyTest {
-    fn do_test(&self, worry_level: i32) -> usize {
+    fn do_test(&self, worry_level: u64) -> usize {
         if worry_level % self.divisibility_operand == 0 {
             self.if_true_receiver
         } else {
@@ -113,7 +113,7 @@ impl FromStr for MonkeyTest {
         let divisibility_operand = test
             .trim()
             .trim_start_matches("Test: divisible by ")
-            .parse::<i32>()?;
+            .parse::<u64>()?;
 
         let if_true_receiver = if_true
             .trim()
@@ -133,19 +133,19 @@ impl FromStr for MonkeyTest {
     }
 }
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 struct Monkey {
-    items: VecDeque<i32>,
+    items: VecDeque<u64>,
     operation: Operation,
     test: MonkeyTest,
 }
 
 impl Monkey {
-    fn receive(&mut self, item: i32) {
+    fn receive(&mut self, item: u64) {
         self.items.push_back(item);
     }
 
-    fn pick_for_inspection(&mut self) -> Option<i32> {
+    fn pick_for_inspection(&mut self) -> Option<u64> {
         self.items.pop_front()
     }
 }
@@ -168,7 +168,7 @@ impl FromStr for Monkey {
             .trim()
             .trim_start_matches("Starting items: ")
             .split(", ")
-            .map(|v| v.parse::<i32>())
+            .map(|v| v.parse::<u64>())
             .collect::<Result<VecDeque<_>, _>>()?;
 
         let (operation, test) = rest
@@ -208,11 +208,16 @@ impl FromStr for Problem {
 fn do_your_business(monkeys: &mut [Monkey], rounds: usize, worry_decay: bool) -> Vec<u64> {
     let mut inspected_items = vec![0u64; monkeys.len()];
 
+    let least_common_multiple = monkeys
+        .iter()
+        .map(|m| m.test.divisibility_operand)
+        .product::<u64>();
+
     for _ in 0..rounds {
         for i in 0..monkeys.len() {
             let monkey = &mut monkeys[i];
 
-            let mut items_to_throw: Vec<(usize, i32)> = vec![];
+            let mut items_to_throw: Vec<(usize, u64)> = vec![];
 
             while let Some(item) = monkey.pick_for_inspection() {
                 inspected_items[i] += 1;
@@ -220,8 +225,14 @@ fn do_your_business(monkeys: &mut [Monkey], rounds: usize, worry_decay: bool) ->
                 // Monkey inspects item
                 let item = monkey.operation.execute(item);
 
-                // Worry level shrinks after inspection
-                let item = if worry_decay { item / 3 } else { item };
+                let item = if worry_decay {
+                    // Worry level shrinks after inspection
+                    item / 3
+                } else {
+                    // Modulo by number that will not affect divisibility checks
+                    // by divisor of any monkey
+                    item % least_common_multiple
+                };
 
                 // Throw item to another monkey
                 let monkey_idx_to_throw_item_to = monkey.test.do_test(item);
@@ -253,14 +264,19 @@ fn main() -> Result<(), anyhow::Error> {
     let input_file_path = get_arg(1).context("pass path to input file as first argument")?;
     let input_string = read_file_to_string(&input_file_path)?;
 
-    let Problem { mut monkeys } = input_string.parse()?;
-    let inspected_items = do_your_business(&mut monkeys, 20, true);
+    let Problem { monkeys } = input_string.parse()?;
+    let inspected_items = do_your_business(&mut monkeys.clone(), 20, true);
 
     println!(
         "Part 1 solution: {}",
         score_monkey_business(&inspected_items)
     );
-    println!("Part 2 solution: {}", 0);
+
+    let inspected_items = do_your_business(&mut monkeys.clone(), 10000, false);
+    println!(
+        "Part 2 solution: {}",
+        score_monkey_business(&inspected_items)
+    );
 
     Ok(())
 }
@@ -306,7 +322,7 @@ Monkey 3:
     }
 
     #[test]
-    fn test_do_your_business() {
+    fn test_do_your_business_with_worry_decay() {
         let Problem { mut monkeys } = TEST_INPUT.parse().unwrap();
         do_your_business(&mut monkeys, 20, true);
 
@@ -314,6 +330,15 @@ Monkey 3:
         assert_eq!(monkeys[1].items, vec![245, 93, 53, 199, 115]);
         assert_eq!(monkeys[2].items, vec![]);
         assert_eq!(monkeys[3].items, vec![]);
+    }
+
+    #[test]
+    fn test_do_your_business_without_worry_decay() {
+        let Problem { mut monkeys } = TEST_INPUT.parse().unwrap();
+        let inspected_items = do_your_business(&mut monkeys, 10000, false);
+        let score = score_monkey_business(&inspected_items);
+
+        assert_eq!(score, 2713310158);
     }
 
     #[test]
