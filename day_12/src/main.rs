f747c03878dcccd8d03ebdc1f1a32f76f53ab9db
Lysander <lysander6@gmail.com>
2022-12-12 17:15:34 +0100

    don't bother with finding out concrete shortest path as we only care about its length

--- day_12/src/main.rs
+++ day_12/src/main.rs
@@ -1,6 +1,6 @@
 use std::{collections::VecDeque, str::FromStr};
 
-use anyhow::{anyhow, Context};
+use anyhow::{anyhow, bail, Context};
 use common::{get_arg, read_file_to_string};
 
 const START: u8 = 'a' as u8 - 1;
@@ -26,8 +26,6 @@ impl FromStr for Problem {
         let end = find_named_point(&map, 'E' as u8)
             .ok_or_else(|| anyhow!("couldn't find end point coordinates"))?;
         map[end.0][end.1] = END;
-
-        // TODO: might as well store start and end point coordinates
 
         Ok(Problem { map })
     }
@@ -65,25 +63,22 @@ fn get_adjacent_indices(x: usize, y: usize, x_dim: usize, y_dim: usize) -> Vec<(
     v
 }
 
-/// Finds shortest path and returns vector of its indices
-///
-/// Note: result will contain both starting and ending points, so number of
-/// steps taken to reach end from start will be one less than length of returned
-/// vector
+/// Finds shortest path from point labeled `start_point_label` to point labeled
+/// `end_point_label` and returns number of steps needed to one from the other
+/// (i.e. path, if it includes both ends, will have length one greater than
+/// number of steps)
 fn find_shortest_path(
     map: &[Vec<u8>],
     start_point_label: u8,
-) -> Result<Vec<(usize, usize)>, anyhow::Error> {
-    let end_coords = find_named_point(&map, END)
+    end_point_label: u8,
+) -> Result<u32, anyhow::Error> {
+    let end_coords = find_named_point(&map, end_point_label)
         .ok_or_else(|| anyhow!("couldn't find ending point coordinates"))?;
 
     let x_dim = map.len();
     let y_dim = map[0].len();
 
     let mut path_lengths: Vec<Vec<Option<u32>>> = vec![vec![None; map[0].len()]; map.len()];
-
-    // We are looking for starting point with label `start_point_label`
-    let mut start_point = (0, 0);
 
     // We start from the end point
     let mut q = VecDeque::from([(end_coords.0, end_coords.1, 0u32)]);
@@ -92,8 +87,8 @@ fn find_shortest_path(
     while let Some((p_x, p_y, p_path_len)) = q.pop_front() {
         // We are done if we reached point with label `start_point_label`
         if map[p_x][p_y] == start_point_label {
-            start_point = (p_x, p_y);
-            break;
+            // We only care about length of the path, so simply return it
+            return Ok(p_path_len);
         }
 
         // Get all adjacent points
@@ -117,34 +112,7 @@ fn find_shortest_path(
         }
     }
 
-    let mut walk_point = start_point;
-    let mut shortest_path = vec![walk_point];
-
-    while walk_point != end_coords {
-        let (shortest_path_point, _) =
-            get_adjacent_indices(walk_point.0, walk_point.1, x_dim, y_dim)
-                .into_iter()
-                .filter_map(|p| {
-                    if let Some(p_length) = path_lengths[p.0][p.1] {
-                        // When walking shortest path we still need to check if
-                        // adjacent point is accessible from current point
-                        if map[walk_point.0][walk_point.1] >= map[p.0][p.1] - 1 {
-                            Some((p, p_length))
-                        } else {
-                            None
-                        }
-                    } else {
-                        None
-                    }
-                })
-                .min_by_key(|&(_, path_length)| path_length)
-                .ok_or_else(|| anyhow!("no path from point {:?}", start_point))?;
-
-        shortest_path.push(shortest_path_point);
-        walk_point = shortest_path_point;
-    }
-
-    Ok(shortest_path)
+    bail!("couldn't find path to {}", start_point_label as char)
 }
 
 fn main() -> Result<(), anyhow::Error> {
@@ -152,11 +120,11 @@ fn main() -> Result<(), anyhow::Error> {
     let input_string = read_file_to_string(&input_file_path)?;
     let Problem { map } = input_string.parse()?;
 
-    let shortest_path = find_shortest_path(&map, START)?;
-    println!("Part 1 solution: {}", shortest_path.len() - 1);
+    let shortest_path = find_shortest_path(&map, START, END)?;
+    println!("Part 1 solution: {}", shortest_path);
 
-    let shortest_path = find_shortest_path(&map, 'a' as u8)?;
-    println!("Part 2 solution: {}", shortest_path.len() - 1);
+    let shortest_path = find_shortest_path(&map, 'a' as u8, END)?;
+    println!("Part 2 solution: {}", shortest_path);
 
     Ok(())
 }
@@ -194,16 +162,16 @@ abdefghi";
     #[test]
     fn test_find_shortest_path() {
         let Problem { map } = TEST_INPUT.parse().unwrap();
-        let shortest_path = find_shortest_path(&map, START).unwrap();
+        let shortest_path = find_shortest_path(&map, START, END).unwrap();
 
-        assert_eq!(shortest_path.len() - 1, 31);
+        assert_eq!(shortest_path, 31);
     }
 
     #[test]
     fn test_find_shortest_path_from_elevation() {
         let Problem { map } = TEST_INPUT.parse().unwrap();
-        let shortest_path = find_shortest_path(&map, 'a' as u8).unwrap();
+        let shortest_path = find_shortest_path(&map, 'a' as u8, END).unwrap();
 
-        assert_eq!(shortest_path.len() - 1, 29);
+        assert_eq!(shortest_path, 29);
     }
 }
