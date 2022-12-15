528cef6f2eb5c921082c85334c14ae02576b97af
Lysander <lysander6@gmail.com>
2022-12-15 08:47:32 +0100

    init day 15

---
+++ day_15/src/main.rs
@@ -0,0 +1,36 @@
+use anyhow::Context;
+use common::{get_arg, read_file_to_string};
+
+fn main() -> Result<(), anyhow::Error> {
+    let input_file_path = get_arg(1).context("pass path to input file as first argument")?;
+    let input_string = read_file_to_string(&input_file_path)?;
+
+    println!("Part 1 solution: {}", 0);
+    println!("Part 2 solution: {}", 0);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "\
+Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+Sensor at x=9, y=16: closest beacon is at x=10, y=16
+Sensor at x=13, y=2: closest beacon is at x=15, y=3
+Sensor at x=12, y=14: closest beacon is at x=10, y=16
+Sensor at x=10, y=20: closest beacon is at x=10, y=16
+Sensor at x=14, y=17: closest beacon is at x=10, y=16
+Sensor at x=8, y=7: closest beacon is at x=2, y=10
+Sensor at x=2, y=0: closest beacon is at x=2, y=10
+Sensor at x=0, y=11: closest beacon is at x=2, y=10
+Sensor at x=20, y=14: closest beacon is at x=25, y=17
+Sensor at x=17, y=20: closest beacon is at x=21, y=22
+Sensor at x=16, y=7: closest beacon is at x=15, y=3
+Sensor at x=14, y=3: closest beacon is at x=15, y=3
+Sensor at x=20, y=1: closest beacon is at x=15, y=3";
+
+    #[test]
+    fn test_name() {}
+}
