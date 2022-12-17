9fbb7b17df8e0d2899c50ba938ccbb66c76e6c86
Lysander <lysander6@gmail.com>
2022-12-17 15:57:08 +0100

    cleanup

--- day_17/src/main.rs
+++ day_17/src/main.rs
@@ -2,72 +2,15 @@ use anyhow::Context;
 use common::{get_arg, read_file_to_string};
 
 fn get_shapes() -> Vec<(usize, Vec<(usize, usize)>)> {
-    let o = false;
-    let x = true;
-
-    let plank = vec![
-        vec![o],
-        vec![o],
-        vec![x],
-        vec![x],
-        vec![x],
-        vec![x],
-        vec![o],
-    ];
-
     let plank_parts = vec![
         (2, 0), // leftmost part
         (3, 0),
         (4, 0),
         (5, 0), // rightmost part
     ];
-
-    let cross = vec![
-        vec![o, o, o],
-        vec![o, o, o],
-        vec![o, x, o],
-        vec![x, x, x],
-        vec![o, x, o],
-        vec![o, o, o],
-        vec![o, o, o],
-    ];
-
     let cross_parts = vec![(2, 1), (3, 0), (3, 1), (3, 2), (4, 1)];
-
-    let l = vec![
-        vec![o, o, o],
-        vec![o, o, o],
-        vec![x, o, o],
-        vec![x, o, o],
-        vec![x, x, x],
-        vec![o, o, o],
-        vec![o, o, o],
-    ];
-
     let l_parts = vec![(2, 0), (3, 0), (4, 0), (4, 1), (4, 2)];
-
-    let plank_vertical = vec![
-        vec![o, o, o, o],
-        vec![o, o, o, o],
-        vec![x, x, x, x],
-        vec![o, o, o, o],
-        vec![o, o, o, o],
-        vec![o, o, o, o],
-        vec![o, o, o, o],
-    ];
-
     let plank_vertical_parts = vec![(2, 0), (2, 1), (2, 2), (2, 3)];
-
-    let block = vec![
-        vec![o, o],
-        vec![o, o],
-        vec![x, x],
-        vec![x, x],
-        vec![o, o],
-        vec![o, o],
-        vec![o, o],
-    ];
-
     let block_parts = vec![(2, 0), (2, 1), (3, 0), (3, 1)];
 
     vec![
@@ -102,7 +45,6 @@ fn simulate_tetris(instructions: &str, rocks_to_drop: usize) -> (usize, Vec<Vec<
     let mut columns = vec![vec![false; 3]; 7];
     let mut top_of_highest_block = 0usize;
     let mut instructions = instructions.chars().cycle();
-    // let mut instruction_ptr = 0usize;
 
     for (shape_height, rock_parts) in get_shapes().iter().cycle().take(rocks_to_drop) {
         for c in 0..columns.len() {
@@ -114,22 +56,13 @@ fn simulate_tetris(instructions: &str, rocks_to_drop: usize) -> (usize, Vec<Vec<
             .map(|&(col, row)| (col, row + top_of_highest_block + 3))
             .collect::<Vec<_>>();
 
-        // eprintln!("The rock begins falling: {:?}", rock_parts);
-
         'dropping: loop {
-            // Check for next instruction
-            // let instr = instructions.get(instruction_ptr);
-            // instruction_ptr += 1;
-
             match instructions.next() {
                 Some('<') => {
-                    // eprint!("Jet of gas pushes rock left");
                     if rock_parts[0].0 == 0 {
                         // Shape touches leftmost column, can't move further
                         // left
-                        // eprintln!(" but nothing happens")
                     } else {
-                        // eprintln!("");
                         // Check if cell to left of every piece is unoccupied
                         let all_free = rock_parts
                             .iter()
@@ -144,12 +77,9 @@ fn simulate_tetris(instructions: &str, rocks_to_drop: usize) -> (usize, Vec<Vec<
                     }
                 }
                 Some('>') => {
-                    // eprint!("Jet of gas pushes rock right");
                     if rock_parts[rock_parts.len() - 1].0 == 6 {
                         // Shape touches rightmost column
-                        // eprintln!(" but nothing happens")
                     } else {
-                        // eprintln!("");
                         let all_free = rock_parts
                             .iter()
                             .all(|&(col, row)| columns[col + 1][row] == false);
@@ -167,7 +97,6 @@ fn simulate_tetris(instructions: &str, rocks_to_drop: usize) -> (usize, Vec<Vec<
                     // Exhausted all instructions
                 }
             }
-            // eprintln!("{:?}", rock_parts);
 
             // Move down
             let all_free = rock_parts
@@ -175,12 +104,10 @@ fn simulate_tetris(instructions: &str, rocks_to_drop: usize) -> (usize, Vec<Vec<
                 .all(|&(col, row)| row > 0 && columns[col][row - 1] == false);
 
             if all_free {
-                // eprintln!("Rock falls 1 unit");
                 for (_, ref mut row) in rock_parts.iter_mut() {
                     *row -= 1;
                 }
             } else {
-                // eprintln!("Rock comes to rest");
                 // Set stone... in stone
                 for (col, row) in rock_parts {
                     columns[col][row] = true;
@@ -193,7 +120,6 @@ fn simulate_tetris(instructions: &str, rocks_to_drop: usize) -> (usize, Vec<Vec<
 
                 break 'dropping;
             }
-            // eprintln!("{:?}", rock_parts);
         }
     }
 
