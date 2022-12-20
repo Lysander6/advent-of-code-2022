a1fae29d88ed921a081a3dc26b89eae51208901e
Lysander <lysander6@gmail.com>
2022-12-20 11:43:51 +0100

    init day 20

---
+++ day_20/src/main.rs
@@ -0,0 +1,29 @@
+use anyhow::Context;
+use common::{get_arg, read_file_to_string};
+
+fn main() -> Result<(), anyhow::Error> {
+    let input_file_path = get_arg(1).context("pass path to input file as first argument")?;
+    let input_string = read_file_to_string(&input_file_path)?;
+
+    println!("Part 1 solution: {}", 0);
+    println!("Part 2 solution: {}", 0);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str = "\
+1
+2
+-3
+3
+-2
+0
+4";
+
+    #[test]
+    fn test_name() {}
+}
