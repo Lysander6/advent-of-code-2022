96660cd731132a32982557a78173c6f355eccdc5
Lysander <lysander6@gmail.com>
2022-12-21 12:41:51 +0100

    init day 21

---
+++ day_21/src/main.rs
@@ -0,0 +1,37 @@
+use anyhow::Context;
+use common::{get_arg, read_file_to_string};
+
+fn main() -> Result<(), anyhow::Error> {
+    let input_file_path = get_arg(1).context("pass path to input file as first argument")?;
+    let input_string = read_file_to_string(&input_file_path)?;
+
+    println!("Part 1 solution: {}", 0);
+    println!("Part 2 solution: {}", 0);
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &str ="\
+root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32";
+
+    #[test]
+    fn test_name() {}
+}
